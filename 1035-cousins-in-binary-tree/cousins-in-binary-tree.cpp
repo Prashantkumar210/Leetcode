c12b915db93d6b41e8593e663c523d8fe07c1124
Prashant kumar <125298352+Prashantkumar210@users.noreply.github.com>
2025-04-01 23:12:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (92.58%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void find(TreeNode* root, int x, int y, int &xdepth, int &ydepth, int count, int &check){
+        
+        if(!root) return;
+
+        else if(((root->left && root->right) && ((root->left->val == x && root->right->val == y) || (root->left->val == y && root->right->val == x)))){
+            xdepth = -2;
+            ydepth = 0;
+            check = 1;
+            return;
+        }
+        else if(check == 0 && root->val == x){
+            xdepth = count;
+            return;
+        }
+
+        else{
+            if(check == 0 && root->val == y){
+                ydepth = count;
+                return;
+            }
+        }
+
+        count++;
+
+        find(root->left, x, y, xdepth, ydepth, count, check);
+        find(root->right, x, y, xdepth, ydepth, count, check);
+    }
+    bool isCousins(TreeNode* root, int x, int y) {
+        
+        int xdepth = 0;
+        int ydepth = 0;
+        int count = 0;
+        int check = 0;
+
+        if((root->val == x) || (root->val== y)) return false;
+
+        find(root, x, y, xdepth, ydepth, count, check);
+        return xdepth == ydepth;
+    }
+};
