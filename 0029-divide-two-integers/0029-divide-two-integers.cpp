aa595df6f567bcdc8f5b3a8be64e7a00da89beb0
Prashant kumar <125298352+Prashantkumar210@users.noreply.github.com>
2025-09-12 19:23:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (96.45%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         
-       if (dividend == INT_MIN && divisor == -1) return INT_MAX;
-        if (dividend == INT_MIN && divisor == 1) return INT_MIN;
+        if(dividend==INT_MIN && divisor==-1) return INT_MAX;
+        if(dividend==INT_MIN && divisor==1) return INT_MIN;
 
-        long long a = llabs((long long)dividend);
-        long long b = llabs((long long)divisor);
-        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+        long long divi = llabs((long long)dividend);
+        long long divsor = llabs((long long)divisor);
 
-        long long start = 0, end = a, ans = -1;
+        long long s = 0;
+        long long e = divi;
+        long long mid = s +(e-s)/2;
+        long long ans = 0;
 
-        long long mid = start+(end - start)/2;
+        while(s<=e){
 
-        while(start<=end){
-
-            if(mid*b == a){
+            if(mid*divsor==divi){
                 ans = mid;
                 break;
             }
-            else if(mid*b<a){
-
+            else if(mid*divsor<divi){
                 ans = mid;
-                start = mid+1;
+                s = mid +1;
             }
-            else{
-                end = mid-1;
-            }
+            else e = mid-1;
 
-            mid = start+ (end-start)/2 ;
+            mid = s +(e-s)/2;
         }
-
-       return (int)ans*sign;
+        if((dividend<0 && divisor<0) ||(dividend>0 && divisor>0)) return int(ans);
+        ans = (-1)*ans;
+        return (int)ans;
     }
 };
