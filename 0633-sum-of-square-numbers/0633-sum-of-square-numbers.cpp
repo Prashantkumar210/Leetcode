dcb4e3a8d2be1129e6ca9582411720acb18f02fd
Prashant kumar <125298352+Prashantkumar210@users.noreply.github.com>
2025-09-29 01:31:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (21.72%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
 
-    bool find(int first, int s, int e, int &c){
+    // bool find(int first, int s, int e, int &c){
+    //     int mid = s+(e-s)/2;
+    //     while(s<=e){
+    //         long long val = 1LL * first * first + 1LL * mid * mid;
+    //         if(val == c){
+    //             cout<<val<<endl;
+    //             return true;
+    //         }
+    //         else if(val<c) s = mid+1;
+    //         else e = mid-1;
 
-        int mid = s+(e-s)/2;
-
-        while(s<=e){
-            long long val = 1LL * first * first + 1LL * mid * mid;
-
-            if(val == c){
-                cout<<val<<endl;
-                return true;
-            }
-            else if(val<c) s = mid+1;
-            else e = mid-1;
-
-            mid = s+(e-s)/2;
-        }
-
-        return false;
-    }
+    //         mid = s+(e-s)/2;
+    //     }
+    //     return false;
+    // }
 
     bool judgeSquareSum(int c) {
         
-        if(c==0) return true;
-        int last = sqrt(c);
-        for(int i=0; i<=last; i++){
+        // if(c==0) return true;
+        // int last = sqrt(c);
+        // for(int i=0; i<=last; i++){
 
-            bool ans = find(i, 0, last, c);
-            if(ans) return true;
+        //     bool ans = find(i, 0, last, c);
+        //     if(ans) return true;
+        // }
+        // return false;
+
+
+        // optimal solution
+        
+        long long a = 0, b = sqrt(c);
+
+        while(a<=b){
+
+            long long sum = a*a + b*b;
+            if(sum == c) return true;
+            
+            if(sum<c) a++;
+            else --b;
         }
 
         return false;
