b5c5145e148697e52ea36591dc72c069898f8d64
Prashant kumar <125298352+Prashantkumar210@users.noreply.github.com>
2026-02-10 17:35:46 +0530

    Time: 3 ms (88.61%) | Memory: 65.9 MB (22.97%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -1,21 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void Inoreder(TreeNode* root, vector<int>&nums){
+
+    void travel(TreeNode* root, vector<int>&nums){
         if(!root) return;
 
-        Inoreder(root->left, nums);
+        travel(root->left, nums);
         nums.push_back(root->val);
-        Inoreder(root->right, nums);
+        travel(root->right, nums);
     }
 
-    TreeNode* BuildTree(vector<int>&nums, int s, int e){
+    TreeNode* tree(vector<int>&nums, int s, int e){
 
         if(s>e) return NULL;
         int mid = s +(e-s)/2;
 
         TreeNode* root = new TreeNode(nums[mid]);
-        root->left = BuildTree(nums, s, mid-1);
-        root->right = BuildTree(nums, mid+1, e);
+        root->left = tree(nums, s, mid-1);
+        root->right = tree(nums, mid+1, e);
 
         return root;
     }
@@ -23,7 +35,8 @@ public:
     TreeNode* balanceBST(TreeNode* root) {
         vector<int>nums;
 
-        Inoreder(root, nums);
-        return BuildTree(nums, 0, nums.size()-1);
+        travel(root, nums);
+
+        return tree(nums, 0, nums.size()-1);
     }
 };
